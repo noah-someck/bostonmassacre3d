4948a4d52b4f9fea659cb70de7ca61fa3ef87a03
noah-someck <jamessomeck@yahoo.com>
2018-01-22 22:07:43 -0600

    Added button click functionality

--- BostonMassacreUI/Assets/Scripts/CaptainController.cs
+++ BostonMassacreUI/Assets/Scripts/CaptainController.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CaptainController : MonoBehaviour {
 
+	public GameObject frontButton;
+	public GameObject backButton;
+	private bool toggleFront;
+	private Color grayColor;
+	private Color whiteColor;
+
+
 	// Use this for initialization
 	void Start () {
-		
+		toggleFront = false;
+		whiteColor = Color.white;
+		grayColor = Color.gray; 
 	}
 	
 	// Update is called once per frame
@@ -15,10 +25,38 @@ public class CaptainController : MonoBehaviour {
 	}
 
 	public void OnFrontCLicked () {
-		Debug.Log ("a");
+		if (!toggleFront) {
+			toggleFront = true;
+
+			Button tempButton = frontButton.GetComponent<Button> ();
+			ColorBlock colorBlock = tempButton.colors;
+			colorBlock.normalColor = Color.gray;
+			colorBlock.highlightedColor = Color.gray;
+			frontButton.GetComponent<Button> ().colors = colorBlock;
+
+			tempButton = backButton.GetComponent<Button> ();
+			colorBlock = tempButton.colors;
+			colorBlock.normalColor = Color.white;
+			colorBlock.highlightedColor = Color.white;
+			backButton.GetComponent<Button> ().colors = colorBlock;
+		}
 	}
 
-	void OnBehindCLicked () {
+	public void OnBehindCLicked () {
+		if (toggleFront) {
+			toggleFront = false;
 
+			Button tempButton = backButton.GetComponent<Button> ();
+			ColorBlock colorBlock = tempButton.colors;
+			colorBlock.normalColor = Color.gray;
+			colorBlock.highlightedColor = Color.gray;
+			backButton.GetComponent<Button> ().colors = colorBlock;
+
+			tempButton = frontButton.GetComponent<Button> ();
+			colorBlock = tempButton.colors;
+			colorBlock.normalColor = Color.white;
+			colorBlock.highlightedColor = Color.white;
+			frontButton.GetComponent<Button> ().colors = colorBlock;
+		}
 	}
 }
