9c0c93feb6f179ec27942bd17cf4686bf019a2e0
Aidan White <aidan.white36@gmail.com>
2018-01-29 19:31:23 -0600

    Changed slider to increment by 5 from 5 to 100

--- BostonMassacreUI/Assets/Scripts/SliderController.cs
+++ BostonMassacreUI/Assets/Scripts/SliderController.cs
@@ -8,16 +8,17 @@ public class SliderController : MonoBehaviour {
     public GameObject crowdSlider;
     public GameObject currentDisplay;
     //public GameObject CrowdVars;
+    public int increment;
 
 	// Use this for initialization
 	void Start () {
-        crowdSlider.GetComponent<Slider>().value = 50;
-        currentDisplay.GetComponent<Text>().text = "50";
+        crowdSlider.GetComponent<Slider>().value = 10;
+        currentDisplay.GetComponent<Text>().text = "10";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        int val = (int)crowdSlider.GetComponent<Slider>().value;
+        int val = (int)crowdSlider.GetComponent<Slider>().value * increment;
         currentDisplay.GetComponent<Text>().text = val.ToString();
         //CrowdVars.GetComponent<CrowdVars>().setCrowdSize(val);
 	}
