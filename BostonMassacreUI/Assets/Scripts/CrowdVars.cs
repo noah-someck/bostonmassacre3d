c774ae6ed2005306b72b7e9d5ee65eef4396a402
noah-someck <jamessomeck@yahoo.com>
2018-01-29 20:39:18 -0600

    Removed MonoBehaviours from CrowdVars

--- BostonMassacreUI/Assets/Scripts/CrowdVars.cs
+++ BostonMassacreUI/Assets/Scripts/CrowdVars.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrowdVars : MonoBehaviour {
+public class CrowdVars {
 
     private int crowdSize;
     private int captainPosition;
